03788d40d18858fc4d90651a82c2c571ff65f043
MhdAsifPK <muhammddasif@gmail.com>
2024-12-18 15:40:28 +0530

    its have a some error in update funcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import AddBooks from './components/AddBooks'
 
 const App = () => {
--- src/components/AddBooks.jsx
+++ src/components/AddBooks.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../Firebase/firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 
 const AddBooks = () => {
+  // =================THIS FOR STORING INPUT FIELD===================================
   const [bookName, setBookName] = useState("");
   const [price, setPrice] = useState("");
+  // console.log(bookName);
+
+  //================== FOR DISPALYA THE BOOK LIST===================================
+  const [bookList, setBookList] = useState([]);
+  // console.log(bookList);
+  //  =================BOOKS ID FOR STORING THE BOOKS ID'S==========================
+  const [bookId, setBookId] = useState(null);
+
+  //=================== CREATE======================================================
 
   const addBook = (e) => {
     e.preventDefault();
@@ -18,18 +36,62 @@ const AddBooks = () => {
         console.log("book added successfully");
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
       });
   };
+
+  //================= READ & GET===================================================
+  const getBookList = async () => {
+    const BookCollectionRef = collection(db, "books");
+    const data1 = await getDocs(BookCollectionRef);
+    // console.log(data1);
+    const filteredData = data1.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    console.log(filteredData);
+    setBookList(filteredData);
+  };
+
+  //=============== DELETE=======================================================
+
+  const bookDelete = async (id) => {
+    const bookDoc = doc(db, "books", id);
+    await deleteDoc(bookDoc);
+    setBookList(bookList.filter((p) => p.id !== id));
+  };
+
+  //=============== UPDATE=======================================================
+
+  const bookUpdate = () => {
+    const bookDoc = doc(db, "books", bookId);
+    updateDoc(bookDoc, { bookName, price });
+  };
+  //============== BOOK EDIT BUTTON FUNCTION=====================================
+  const bookEdit = async (id) => {
+    const bookDoc = doc(db, "books", id);
+    const booksedit = await getDoc(bookDoc);
+    console.log(booksedit.id);
+    console.log(booksedit.data());
+    setBookName(booksedit.data().bookName);
+    setPrice(booksedit.data().price);
+    setBookId(booksedit.id);
+  };
+  useEffect(() => {
+    getBookList();
+    bookUpdate;
+    // bookDelete();
+  }, [bookName, price]);
 
   return (
     <div>
       <form onSubmit={addBook} className="addBookForm">
-        <label htmlFor="book name">Book Name</label>
+        <label>Book Name</label>
         <input
           type="text"
           name="book name"
           placeholder="Enter Book Name"
+          value={bookName}
           onChange={(e) => setBookName(e.target.value)}
         />
         <label htmlFor="price">Price</label>
@@ -37,10 +99,32 @@ const AddBooks = () => {
           type="text"
           name="price"
           placeholder="Enter Price"
+          value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
         <input type="submit" value="Add Book" />
+        <button onClick={bookUpdate}>Update</button>
       </form>
+
+      {/*=================== BOOK LISST ===============================================*/}
+
+      <div>
+        {bookList.map((booklist) => (
+          <div key={booklist.id}>
+            <h2>BOOK:{booklist.bookName}</h2>
+            <p>price:{booklist.price}</p>
+            <button onClick={() => bookDelete(booklist.id)}>delete</button>
+            <button
+              className="edit"
+              onClick={() => {
+                bookEdit(booklist.id);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
